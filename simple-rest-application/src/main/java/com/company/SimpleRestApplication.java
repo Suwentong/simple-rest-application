783ba9a2c04138dc2b1fee857361339704e928fe
Suwentong <abdukadirov048@gmail.com>
2023-02-15 20:15:40 +0800

    Initial commit

---
+++ simple-rest-application/src/main/java/com/company/SimpleRestApplication.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+public class SimpleRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestApplication.class, args);
+	}
+
+}
