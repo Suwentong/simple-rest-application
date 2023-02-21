b4d4b502ce01a87beda87b2caa88ce5344287f59
Suwentong <abdukadirov048@gmail.com>
2023-02-21 14:18:02 +0800

    Connected Postgre, Added Message class

---
+++ simple-rest-application/src/main/java/com/company/domain/Message.java
@@ -0,0 +1,32 @@
+package com.company.domain;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Table
+@ToString(of = {"id", "text"})
+@EqualsAndHashCode(of = {"id"})
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String text;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
